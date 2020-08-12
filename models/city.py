dee59698d16979039a44838d5ea73378a1070da8
linkjavier <linkjavier@hotmail.com>
2020-08-12 18:43:04 -0500

    Updated place.py city.py sqlalchemy connection

--- models/city.py
+++ models/city.py
@@ -5,14 +5,22 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from models.place import Place
+import os
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-   
-    __tablename__ = 'cities'
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-   
-    states_a = relationship("State", backref="cities_a")
-                            
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete-orphan")
+
+    else:
+        name = ""
+        state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes City"""
+        super().__init__(*args, **kwargs)
