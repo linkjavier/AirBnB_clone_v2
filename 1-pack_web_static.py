dcf489c11c329d0002d2a33ef1e37f7dceff36d5
linkjavier <linkjavier@hotmail.com>
2020-08-17 18:14:43 -0500

    Added Script to save the Web_proyect in a .tgz file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack.
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+        Function that save the Web_proyect in a .tgz file
+        with actual date on name.
+    """
+    now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    filename = "version/web_static_{}.tgz".format(now)
+    try:
+        local("mkdir -p ./versions")
+        local("tar --create --verbose -z --file={} ./web_static"
+              .format(filename))
+        return filename
+    except:
+        return None
