548a8039c262eefddc531fa5c310ec0b16503974
linkjavier <linkjavier@hotmail.com>
2020-08-17 21:47:57 -0500

    Added sudo commands to 2-do_deploy_web_static.py Script to deploy the Web Static on the server a

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,32 +5,32 @@
  using the function do_deploy.
 """
 from fabric.api import *
-from fabric.operations import run, put
+from fabric.operations import run, put, sudo
 import os
 env.hosts = ['35.229.26.94', '184.72.156.146']
 
 
-def do_deploy(filepath):
+def do_deploy(archive_path):
     """
         Function to deploy the Web Static on the server
     """
-    if os.path.isfile(filepath) is False:
+    if os.path.isfile(archive_path) is False:
         return False
     try:
-        filename = filepath.split("/")[-1]
+        filename = archive_path.split("/")[-1]
         path = "/data/web_static/releases"
-        put("{}".format(filepath), "/tmp/{}".format(filename))
+        put("{}".format(archive_path), "/tmp/{}".format(filename))
         foldername = filename.split(".")
-        run("mkdir -p {}/{}/".format(path, foldername[0]))
+        run("sudo mkdir -p {}/{}/".format(path, foldername[0]))
         newfile = '.'.join(foldername)
-        run("tar -xzf /tmp/{} -C {}/{}/"
+        run("sudo tar -xzf /tmp/{} -C {}/{}/"
             .format(newfile, path, foldername[0]))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}/{}/web_static/* {}/{}/"
+        run("sudo rm /tmp/{}".format(filename))
+        run("sudo mv {}/{}/web_static/* {}/{}/"
             .format(path, foldername[0], path, foldername[0]))
-        run("rm -rf {}/{}/web_static".format(path, foldername[0]))
-        run("rm -rf /data/web_static/current")
-        run("ln -sf {}/{} /data/web_static/current"
+        run("sudo rm -rf {}/{}/web_static".format(path, foldername[0]))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -sf {}/{} /data/web_static/current"
             .format(path, foldername[0]))
         return True
     except:
