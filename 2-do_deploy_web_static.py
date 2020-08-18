19ce6f350d69993960b2be6ef05e43d663ceff23
linkjavier <linkjavier@hotmail.com>
2020-08-17 22:09:18 -0500

    Added sudo commands to 2-do_deploy_web_static.py Script to deploy the Web Static on the server a

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@
  that distributes an archive to your web servers,
  using the function do_deploy.
 """
-from fabric.api import *
+from fabric.api import env
 from fabric.operations import run, put, sudo
 import os
 env.hosts = ['35.229.26.94', '184.72.156.146']
@@ -19,16 +19,20 @@ def do_deploy(archive_path):
     try:
         filename = archive_path.split("/")[-1]
         path = "/data/web_static/releases"
-        put("{}".format(archive_path), "/tmp/{}".format(filename))
+        put("{}".format(archive_path), "/tmp/{}"
+            .format(filename))
         foldername = filename.split(".")
-        run("sudo mkdir -p {}/{}/".format(path, foldername[0]))
+        run("sudo mkdir -p {}/{}/"
+            .format(path, foldername[0]))
         newfile = '.'.join(foldername)
         run("sudo tar -xzf /tmp/{} -C {}/{}/"
             .format(newfile, path, foldername[0]))
-        run("sudo rm /tmp/{}".format(filename))
+        run("sudo rm /tmp/{}"
+            .format(filename))
         run("sudo mv {}/{}/web_static/* {}/{}/"
             .format(path, foldername[0], path, foldername[0]))
-        run("sudo rm -rf {}/{}/web_static".format(path, foldername[0]))
+        run("sudo rm -rf {}/{}/web_static"
+            .format(path, foldername[0]))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -sf {}/{} /data/web_static/current"
             .format(path, foldername[0]))
