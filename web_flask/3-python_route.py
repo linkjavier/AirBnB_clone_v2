9eb731d36544ef0f7803933f2f656e0b092697ef
linkjavier <linkjavier@hotmail.com>
2020-09-03 01:03:28 -0500

    Added 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+    Script that starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def return_hello():
+    """
+        Function that returns Hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def return_hbnb():
+    """
+        Function that returns HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def return_last(text):
+    """
+        Display “C ” followed by the value of the text variable
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def return_python(text="is cool"):
+    """
+        Displays Python, followed by the value of the text variable
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
