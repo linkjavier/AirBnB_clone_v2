1c95ddadeab1276b6dd72b5b3eda919e65c305b2
linkjavier <linkjavier@hotmail.com>
2020-09-03 00:43:04 -0500

    Added 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    Script that starts a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def return_hello():
+    """
+        Function that returns Hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def return_hbnb():
+    """
+        Function that returns HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def return_last(text):
+    """
+        Display “C ” followed by the value of the text variable
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
